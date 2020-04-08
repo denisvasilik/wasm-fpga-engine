1052ca72640e2d035cac7cba24be5466504db864
Denis Vasilik <contact@denisvasilik.com>
2020-04-08 23:20:59 +0200

    Add WasmFpgaWishbone

--- scripts/create_project.tcl
+++ scripts/create_project.tcl
@@ -59,6 +59,7 @@ set obj [get_filesets sources_1]
 set files_vhd [list \
  [file normalize "${project_src}/WasmFpgaEngine.vhd" ]\
  [file normalize "${project_src}/WasmFpgaEnginePackage.vhd" ]\
+ [file normalize "${project_src}/WasmFpgaWishbone.vhd" ]\
  [file normalize "${project_src_gen}/vhd_gen/header/wasm_fpga_engine_header.vhd" ]\
  [file normalize "${project_src_gen}/vhd_gen/wishbone/wasm_fpga_engine_wishbone.vhd" ]\
  [file normalize "${project_package}/component.xml" ]\

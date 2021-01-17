647543cfdeceafcd06d72815be6bc81809136fb7
Denis Vasilik <contact@denisvasilik.com>
2021-01-17 10:50:50 +0100

    Remove UART dependency

--- src/WasmFpgaEnginePackage.vhd
+++ src/WasmFpgaEnginePackage.vhd
@@ -218,6 +218,7 @@ package WasmFpgaEnginePackage is
     constant State14 : std_logic_vector(15 downto 0) := x"000F";
     constant State15 : std_logic_vector(15 downto 0) := x"0010";
     constant State16 : std_logic_vector(15 downto 0) := x"0011";
+
     constant StateEnd : std_logic_vector(15 downto 0) := x"00F0";
     constant StateTrapped : std_logic_vector(15 downto 0) := x"00FD";
     constant StateNotSupported : std_logic_vector(15 downto 0) := x"00FE";
@@ -379,6 +380,35 @@ package WasmFpgaEnginePackage is
     procedure SetLocalFromStack(signal State : inout std_logic_vector;
                                 signal WasmFpgaInstruction_WasmFpgaStack : out T_WasmFpgaInstruction_WasmFpgaStack;
                                 signal WasmFpgaStack_WasmFpgaInstruction : in T_WasmFpgaStack_WasmFpgaInstruction);
+
+    -- UART IO
+    constant MAX_STATE_HIGH_IDX : natural  := 7;
+    constant MAX_STRING_HIGH_IDX : natural := 127;
+
+    constant println_stateIdle : unsigned(MAX_STATE_HIGH_IDX downto 0) := to_unsigned(0,MAX_STATE_HIGH_IDX+1);
+    constant println_stateExit : unsigned(MAX_STATE_HIGH_IDX downto 0) := to_unsigned(1,MAX_STATE_HIGH_IDX+1);
+    constant println_stateError : unsigned(MAX_STATE_HIGH_IDX downto 0) := to_unsigned(255,MAX_STATE_HIGH_IDX+1);
+
+   procedure println (constant value : in string;
+                      signal state : inout unsigned(MAX_STATE_HIGH_IDX downto 0);
+                      signal count : inout natural range 0 to MAX_STRING_HIGH_IDX;
+                      signal Uart_Adr : out std_logic_vector(23 downto 0);
+                      signal Uart_Sel : out std_logic_vector(3 downto 0);
+                      signal Uart_DatIn: in std_logic_vector(31 downto 0);
+                      signal Uart_We : out std_logic;
+                      signal Uart_Stb : out std_logic;
+                      signal Uart_Cyc : out std_logic_vector(0 downto 0);
+                      signal Uart_DatOut : out std_logic_vector(31 downto 0);
+                      signal Uart_Ack : in std_logic);
+
+    constant WASMFPGAUART_ADR_BLK_BASE_UartBlk : std_logic_vector(23 downto 0) := x"000000";
+    constant WASMFPGAUART_ADR_TxDataReg : std_logic_vector(23 downto 0) := std_logic_vector(x"000008" + unsigned(WASMFPGAUART_ADR_BLK_BASE_UartBlk));
+
+    constant WASMFPGAUART_ADR_ControlReg : std_logic_vector(23 downto 0) := std_logic_vector(x"000000" + unsigned(WASMFPGAUART_ADR_BLK_BASE_UartBlk));
+    constant WASMFPGAUART_VAL_TxDoRun : std_logic := '1';
+
+    constant WASMFPGAUART_ADR_StatusReg : std_logic_vector(23 downto 0) := std_logic_vector(x"000004" + unsigned(WASMFPGAUART_ADR_BLK_BASE_UartBlk));
+    constant WASMFPGAUART_VAL_TxIsNotBusy : std_logic := '0';
 
 end;
 
@@ -947,5 +977,93 @@ package body WasmFpgaEnginePackage is
             return x"00000000";
         end if;
     end;
+
+   procedure println (constant value : in string;
+                      signal state : inout unsigned(MAX_STATE_HIGH_IDX downto 0);
+                      signal count : inout integer range 0 to MAX_STRING_HIGH_IDX;
+                      signal Uart_Adr : out std_logic_vector(23 downto 0);
+                      signal Uart_Sel : out std_logic_vector(3 downto 0);
+                      signal Uart_DatIn: in std_logic_vector(31 downto 0);
+                      signal Uart_We : out std_logic;
+                      signal Uart_Stb : out std_logic;
+                      signal Uart_Cyc : out std_logic_vector(0 downto 0);
+                      signal Uart_DatOut : out std_logic_vector(31 downto 0);
+                      signal Uart_Ack : in std_logic) is
+       constant stateTransmit0 : unsigned(MAX_STATE_HIGH_IDX downto 0) := to_unsigned(2,MAX_STATE_HIGH_IDX+1);
+       constant stateTransmit1 : unsigned(MAX_STATE_HIGH_IDX downto 0) := to_unsigned(5,MAX_STATE_HIGH_IDX+1);
+       constant stateTransmit2 : unsigned(MAX_STATE_HIGH_IDX downto 0) := to_unsigned(3,MAX_STATE_HIGH_IDX+1);
+       constant stateTransmit3 : unsigned(MAX_STATE_HIGH_IDX downto 0) := to_unsigned(6,MAX_STATE_HIGH_IDX+1);
+       constant stateTransmit4 : unsigned(MAX_STATE_HIGH_IDX downto 0) := to_unsigned(7,MAX_STATE_HIGH_IDX+1);
+       constant stateTransmit5 : unsigned(MAX_STATE_HIGH_IDX downto 0) := to_unsigned(8,MAX_STATE_HIGH_IDX+1);
+       constant stateTransmit6 : unsigned(MAX_STATE_HIGH_IDX downto 0) := to_unsigned(9,MAX_STATE_HIGH_IDX+1);
+   begin
+       case state is
+           when println_stateIdle =>
+               count <= 1;
+               state <= stateTransmit0;
+           when stateTransmit0 =>
+                Uart_Cyc <= "1";
+                Uart_Stb <= '1';
+                Uart_Sel <= (others => '1');
+                Uart_We <= '1';
+                Uart_Adr <= WASMFPGAUART_ADR_TxDataReg;
+                Uart_DatOut <= std_logic_vector(to_unsigned(character'pos(value(count)), Uart_DatOut'LENGTH));
+                state <= stateTransmit1;
+           when stateTransmit1 =>
+                if ( Uart_Ack = '1' ) then
+                    Uart_Cyc <= (others => '0');
+                    Uart_Stb <= '0';
+                    Uart_Adr <= (others => '0');
+                    Uart_Sel <= (others => '0');
+                    state <= stateTransmit2;
+                end if;
+           when stateTransmit2 =>
+                Uart_Cyc <= "1";
+                Uart_Stb <= '1';
+                Uart_Sel <= (others => '1');
+                Uart_We <= '1';
+                Uart_Adr <= WASMFPGAUART_ADR_ControlReg;
+                Uart_DatOut <= (31 downto 1 => '0') & WASMFPGAUART_VAL_TxDoRun;
+                state <= stateTransmit3;
+           when stateTransmit3 =>
+                if ( Uart_Ack = '1' ) then
+                    Uart_Cyc <= (others => '0');
+                    Uart_Stb <= '0';
+                    Uart_Adr <= (others => '0');
+                    Uart_Sel <= (others => '0');
+                    state <= stateTransmit4;
+                end if;
+           when stateTransmit4 =>
+                Uart_Cyc <= "1";
+                Uart_Stb <= '1';
+                Uart_Sel <= (others => '1');
+                Uart_We <= '1';
+                Uart_Adr <= WASMFPGAUART_ADR_StatusReg;
+                state <= stateTransmit5;
+           when stateTransmit5 =>
+                if ( Uart_Ack = '1' ) then
+                    Uart_Cyc <= "0";
+                    Uart_Stb <= '0';
+                    Uart_Adr <= (others => '0');
+                    Uart_Sel <= (others => '0');
+                    if (Uart_DatIn(0) = WASMFPGAUART_VAL_TxIsNotBusy) then
+                        state <= stateTransmit6;
+                    else
+                        state <= stateTransmit4;
+                    end if;
+                end if;
+           when stateTransmit6 =>
+               if (count = value'LENGTH) then
+                   state <= println_stateExit;
+               else
+                   count <= count + 1;
+                   state <= stateTransmit0;
+               end if;
+           when println_stateExit =>
+                state <= println_stateExit;
+           when others =>
+               state  <= println_stateError;
+       end case;
+	end;
 
 end;
--- tb/tb_WasmFpgaEngine.vhd
+++ tb/tb_WasmFpgaEngine.vhd
@@ -182,14 +182,6 @@ architecture behavioural of tb_WasmFpgaEngine is
             Bus_DatIn: in std_logic_vector(31 downto 0);
             Bus_Ack : in std_logic;
             Bus_Cyc : out std_logic_vector(0 downto 0);
-            Uart_Adr : out std_logic_vector(23 downto 0);
-            Uart_Sel : out std_logic_vector(3 downto 0);
-            Uart_DatIn: in std_logic_vector(31 downto 0);
-            Uart_We : out std_logic;
-            Uart_Stb : out std_logic;
-            Uart_Cyc : out std_logic_vector(0 downto 0);
-            Uart_DatOut : out std_logic_vector(31 downto 0);
-            Uart_Ack : in std_logic;
             Trap : out std_logic
         );
     end component;
@@ -421,14 +413,6 @@ begin
             Bus_DatIn => WasmFpgaBus_WasmFpgaEngine.DatOut,
             Bus_Ack => WasmFpgaBus_WasmFpgaEngine.Ack,
             Bus_Cyc => WasmFpgaEngine_WasmFpgaBus.Cyc,
-            Uart_Adr => open,
-            Uart_Sel => open,
-            Uart_DatIn => (others => '0'),
-            Uart_We => open,
-            Uart_Stb => open,
-            Uart_Cyc => open,
-            Uart_DatOut => open,
-            Uart_Ack => '0',
             Trap => open
        );
 

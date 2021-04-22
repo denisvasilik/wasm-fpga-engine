33e9ee83cc469edddea3b15f2f0aead33769a57e
Denis Vasilik <contact@denisvasilik.com>
2021-04-22 21:07:13 +0200

    Refactor end instruction

--- src/instructions/End.vhd
+++ src/instructions/End.vhd
@@ -15,40 +15,39 @@ entity InstructionEnd is
     port (
         Clk : in std_logic;
         nRst : in std_logic;
-        FromWasmFpgaInvocation : in T_WasmFpgaInvocation_WasmFpgaInstruction;
-        ToWasmFpgaInvocation : out T_WasmFpgaInstruction_WasmFpgaInvocation;
-        FromWasmFpgaStack : in T_WasmFpgaStack_WasmFpgaInstruction;
-        ToWasmFpgaStack : out T_WasmFpgaInstruction_WasmFpgaStack;
-        FromWasmFpgaModuleRam : in T_WasmFpgaModuleRam_WasmFpgaInstruction;
-        ToWasmFpgaModuleRam : buffer T_WasmFpgaInstruction_WasmFpgaModuleRam;
-        FromWasmFpgaMemory : in T_WasmFpgaMemory_WasmFpgaInstruction;
-        ToWasmFpgaMemory : out T_WasmFpgaInstruction_WasmFpgaMemory
+        ToWasmFpgaInstruction : in T_ToWasmFpgaInstruction;
+        FromWasmFpgaInstruction : out T_FromWasmFpgaInstruction;
+        FromWasmFpgaStack : in T_FromWasmFpgaStack;
+        ToWasmFpgaStack : out T_ToWasmFpgaStack;
+        FromWasmFpgaModuleRam : in T_FromWasmFpgaModuleRam;
+        ToWasmFpgaModuleRam : buffer T_ToWasmFpgaModuleRam;
+        FromWasmFpgaMemory : in T_FromWasmFpgaMemory;
+        ToWasmFpgaMemory : out T_ToWasmFpgaMemory
     );
 end;
 
-architecture InstructionEndArchitecture of InstructionEnd is
+architecture Behavioural of InstructionEnd is
 
-    signal Rst : std_logic;
     signal State : std_logic_vector(15 downto 0);
     signal ActivationFrameStackState : std_logic_vector(15 downto 0);
 
 begin
 
-    Rst <= not nRst;
+    ToWasmFpgaMemory <= (
+        Run => '0',
+        Address => (others => '0'),
+        WriteData => (others => '0'),
+        WriteEnable => '0'
+    );
 
-    ToWasmFpgaMemory.Run <= '0';
-    ToWasmFpgaMemory.Address <= (others => '0');
-    ToWasmFpgaMemory.WriteData <= (others => '0');
-    ToWasmFpgaMemory.WriteEnable <= '0';
-
-    -- Module not used
-    ToWasmFpgaModuleRam.Run <= '0';
-    ToWasmFpgaModuleRam.Address <= (others => '0');
+    ToWasmFpgaModuleRam <= (
+        Run => '0',
+        Address => (others => '0')
+    );
 
     process (Clk, Rst) is
     begin
-        if (Rst = '1') then
-          -- Stack
+        if (nRst = '0') then
           ToWasmFpgaStack <= (
               Run => '0',
               Action => (others => '0'),
@@ -61,18 +60,19 @@ begin
               ModuleInstanceUid => (others => '0'),
               LocalIndex => (others => '0')
           );
-          -- Invocation
-          ToWasmFpgaInvocation.Address <= (others => '0');
-          ToWasmFpgaInvocation.Trap <= '0';
-          ToWasmFpgaInvocation.Busy <= '1';
+          FromWasmFpgaInstruction <= (
+              Address => (others => '0'),
+              Trap => '0',
+              Busy => '1'
+          );
           ActivationFrameStackState <= StateIdle;
           State <= StateIdle;
         elsif rising_edge(Clk) then
             if (State = StateIdle) then
-                ToWasmFpgaInvocation.Busy <= '0';
-                if (FromWasmFpgaInvocation.Run = '1') then
-                    ToWasmFpgaInvocation.Busy <= '1';
-                    ToWasmFpgaInvocation.Address <= FromWasmFpgaInvocation.Address;
+                FromWasmFpgaInstruction.Busy <= '0';
+                if (ToWasmFpgaInstruction.Run = '1') then
+                    FromWasmFpgaInstruction.Busy <= '1';
+                    FromWasmFpgaInstruction.Address <= ToWasmFpgaInstruction.Address;
                     State <= State0;
                 end if;
             elsif(State = State0) then
@@ -80,11 +80,11 @@ begin
                                       ToWasmFpgaStack,
                                       FromWasmFpgaStack);
                 if(ActivationFrameStackState = StateEnd) then
-                    ToWasmFpgaInvocation.Address <= FromWasmFpgaStack.ReturnAddress(23 downto 0);
+                    FromWasmFpgaInstruction.Address <= FromWasmFpgaStack.ReturnAddress(23 downto 0);
                     State <= StateIdle;
                 end if;
             end if;
         end if;
     end process;
 
-end architecture;
+end;
